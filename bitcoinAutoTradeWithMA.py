b433f19d8dcad46d4e2ef72aa1b62aba4d961742
Yunjae-Lee <yunjea0312@mailplug.co.kr>
2021-05-09 14:53:03 +0900

    db 연결중

--- bitcoinAutoTradeWithMA.py
+++ bitcoinAutoTradeWithMA.py
@@ -2,6 +2,7 @@ import time
 import pyupbit
 import datetime
 import requests
+import sqlite3
 
 r = open('tokens.txt', mode='rt', encoding='utf-8')
 tokens = r.readlines()
@@ -50,6 +51,28 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
+def get_daily_coin(coin_id):
+    conn = sqlite3.connect("upbit.dbs", isolation_level=None)
+    # 커서 획득
+    c = conn.cursor()
+    c.execute("SELECT * FROM daily_buy_list WHERE id=:id1", {"id1": coin_id})
+    conn.close()
+
+def update_daily_coin(data):
+    conn = sqlite3.connect("upbit.dbs", isolation_level=None)
+    # 커서 획득
+    c = conn.cursor()
+    c.execute("SELECT * FROM daily_buy_list WHERE id=:id1", {"id1": coin_id})
+    conn.close()
+
+def insert_daily_coin(data):
+    conn = sqlite3.connect("upbit.dbs", isolation_level=None)
+    # 커서 획득
+    c = conn.cursor()
+    c.execute("INSERT INTO daily_buy_list VALUES(?,?,?,?)", data)
+    conn.close()
+
+
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
@@ -63,22 +86,37 @@ while True:
             now = datetime.datetime.now()
             start_time = get_start_time(coin)
             end_time = start_time + datetime.timedelta(days=1)
+            #print(now)
+            #print(start_time)
+            #print(end_time)
+            print(coin)
     
             #하루단위
             if start_time < now < end_time - datetime.timedelta(seconds=10):
+                #print('if!!!')
                 target_price = get_target_price(coin, 0.5)
                 daily_ma = get_daily_ma(coin, 10)
                 current_price = get_current_price(coin)
+                #print(target_price)
+                #print(daily_ma)
+                #print(current_price)
                 if target_price < current_price and daily_ma < current_price:
                     krw = get_balance("KRW")
                     if krw > 5100:
-                        buy_result = upbit.buy_market_order(coin, krw*0.9995)
+                        print(krw)
+                        print('산다!!!!')
+                        #buy_result = upbit.buy_market_order(coin, krw*0.9995)
+                        buy_result = '10000'
                         post_message(myToken, "#coin", coin + "buy : "+str(buy_result))
             else:
+                print('else!!')
                 sell_coin = get_balance(coin)
+                print(sell_coin)
                 if sell_coin > 0.00008:
-                    sell_result = upbit.sell_market_order(coin, sell_coin*0.9995)
-                    post_message(myToken, "#coin", coin + " sell : "+str(buy_result))
+                    print('sell_coin if!!!!')
+                    #sell_result = upbit.sell_market_order(coin, sell_coin*0.9995)
+                    sell_result = '10000'
+                    post_message(myToken, "#coin", coin + " sell : "+str(sell_result))
             time.sleep(1)
         except Exception as e:
             print(e)
--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 import pyupbit
+import sqlite3
 
 access = "aQuzJPeK4X9npocwuD8QXa7ApwL5jgxSioq0O3x6"
 secret = "KGE3D0lQQG8wL5OR3HI5BiSCnSuhyNfxZof0IN7j"
 upbit = pyupbit.Upbit(access, secret)
 
+conn = sqlite3.connect("upbit.dbs", isolation_level=None)
+
+# 커서 획득
+c = conn.cursor()
+
+c.execute("SELECT * FROM daily_buy_list")
+print(c.fetchone())
+
+# 방법 1
+#c.execute("SELECT * FROM table1 WHERE id=:id1 OR id=:id2", {"id1": 1, "id2": 4})
+c.execute("SELECT * FROM table1")
+for row in c.fetchall():
+    print(row)
+
 #df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
 #print(df.index[0])
-print(upbit.get_balance("TON"))
+#print(upbit.get_balance("TON"))
 #print(upbit.get_balance("KRW-BTC"))
